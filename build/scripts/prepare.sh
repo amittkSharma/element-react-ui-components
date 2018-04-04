a4f2090b0890d5b2a1dadbc3b847ea03969f3aa7
Amit Sharma <amit12.sharma@gmail.com>
2018-04-04 07:38:36 -0700

    fix: publishing issue to npm

--- build/scripts/prepare.sh
+++ build/scripts/prepare.sh
@@ -6,7 +6,7 @@ rm -fr dist
 babel src --out-dir dist/npm/es5/src --copy-files
 babel libs --out-dir dist/npm/es5/libs --copy-files
 babel build/npm/index.js --out-file dist/npm/es5/index.js --copy-files
-#export BABEL_ENV=production
+export BABEL_ENV=production
 babel src --out-dir dist/npm/es6/src --copy-files
 babel libs --out-dir dist/npm/es6/libs --copy-files
 # keep es6 for next.js
